617a169eae0d391e4f637013497ca20116a5871c
Shinbo-Rina <b1915499@planet.kanazawa-it.ac.jp>
2021-12-17 06:34:14 +0900

    ハッカソン終了

--- app/src/main/java/com/example/hackathon/AreaActivity.java
+++ app/src/main/java/com/example/hackathon/AreaActivity.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
 
@@ -19,25 +22,44 @@ public class AreaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_area);
 
         // ヘッダーの設定
-        Toolbar areaTB = findViewById(R.id.seat_tb);
+        Toolbar areaTB = findViewById(R.id.app_tb);
         setSupportActionBar(areaTB);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         // 次へボタンの設定
         Button nextBtn = findViewById(R.id.next_btn);
-        nextBtn.setOnClickListener(new AreaActivity.NextClickListener());
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // ラジオボタンの処理
+                RadioGroup areaRG = findViewById(R.id.area_rg);
+                int check = areaRG.getCheckedRadioButtonId();
+                String selectArea = "";
+                if (check > 0) {
+                    switch (check) {
+                        case R.id.r_rbtn:
+                            selectArea = "r";
+                            break;
+                        case R.id.l2_rbtn:
+                            selectArea = "l2";
+                            break;
+                        case R.id.l1_rbtn:
+                            selectArea = "l1";
+                            break;
+                    }
+                    Intent it = new Intent(getApplicationContext(), SeatActivity.class);
+                    it.putExtra("AREA", selectArea);
+                    startActivityForResult(it, 1);
+                } else {
+                    Toast.makeText(AreaActivity.this, "どれか選択してください", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
     @Override
     public boolean onSupportNavigateUp() {
         finish();
         return super.onSupportNavigateUp();
-    }
-
-    class NextClickListener implements View.OnClickListener {
-        public void onClick(View v) {
-            Intent it = new Intent(getApplicationContext(), SeatActivity.class);
-            startActivityForResult(it, 1);
-        }
     }
 }
--- app/src/main/java/com/example/hackathon/SeatActivity.java
+++ app/src/main/java/com/example/hackathon/SeatActivity.java
@@ -4,23 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.FileProvider;
 
-import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationBarView;
+
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.InputStream;
+import java.util.Locale;
 
 // seat画面
 
@@ -31,38 +32,79 @@ public class SeatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seat);
 
+        // 値を受け取る
+        Intent it = getIntent();
+        final String selectArea = it.getStringExtra("AREA");
+
+        // シート画像とシェア画像の初期値を変える
+        ImageView seatNumIV = findViewById(R.id.seat_num_iv);
+        int areaId = getResources().getIdentifier("com.example.hackathon:drawable/seat_" + selectArea + "_num", null, null);
+        seatNumIV.setImageResource(areaId);
+        ImageView shareIV = findViewById(R.id.share_iv);
+        int shareId = getResources().getIdentifier("com.example.hackathon:drawable/share_" + selectArea + "0", null, null);
+        shareIV.setImageResource(shareId);
+
+        // スピナーを変える
+        Spinner seatSp = findViewById(R.id.seat_sp);
+        String[] r_list = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"};
+        String[] l2_list = {"A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8"};
+        String[] l1_list = {"A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8","B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "E1", "E2", "E3", "E4", "E5", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, r_list);
+        switch (selectArea) {
+            case "l2":
+                adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, l2_list);
+                break;
+            case "l1":
+                adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, l1_list);
+                break;
+        }
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        seatSp.setAdapter(adapter);
+
+        // シェア画像の更新
+        seatSp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String item = (String)seatSp.getSelectedItem();
+                int shareId = getResources().getIdentifier("com.example.hackathon:drawable/share_" + selectArea + item.toLowerCase(), null, null);
+                shareIV.setImageResource(shareId);
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
         // ヘッダーの設定
-        Toolbar areaTB = findViewById(R.id.seat_tb);
+        Toolbar areaTB = findViewById(R.id.app_tb);
         setSupportActionBar(areaTB);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         // 次へボタンの設定
         Button shareBtn = findViewById(R.id.share_btn);
-        shareBtn.setOnClickListener(new SeatActivity.ShareClickListener());
+        shareBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ImageView image = findViewById(R.id.share_iv);
+
+                BitmapDrawable bitmapDrawable = (BitmapDrawable) image.getDrawable();
+                Bitmap bitmap = bitmapDrawable.getBitmap();
+
+                Uri uri = getmageToShare(bitmap);
+                Intent shareIntent = new Intent(Intent.ACTION_VIEW);
+                shareIntent.setAction(Intent.ACTION_SEND);
+                shareIntent.putExtra(Intent.EXTRA_STREAM, uri);
+                shareIntent.setType("image/png");
+                startActivity(Intent.createChooser(shareIntent, "Share Via"));
+            }
+        });
     }
 
     @Override
     public boolean onSupportNavigateUp() {
         finish();
         return super.onSupportNavigateUp();
-    }
-
-    // 次へボタンを押したときの処理
-    class ShareClickListener implements View.OnClickListener {
-        public void onClick(View v) {
-            ImageView image = findViewById(R.id.image_im);
-
-            BitmapDrawable bitmapDrawable = (BitmapDrawable) image.getDrawable();
-            Bitmap bitmap = bitmapDrawable.getBitmap();
-
-            Uri uri = getmageToShare(bitmap);
-            Intent shareIntent = new Intent(Intent.ACTION_VIEW);
-            shareIntent.setAction(Intent.ACTION_SEND);
-            shareIntent.putExtra(Intent.EXTRA_STREAM, uri);
-            shareIntent.setType("image/png");
-            startActivity(Intent.createChooser(shareIntent, "Share Via"));
-        }
     }
 
     // bitmapからuriを生成
